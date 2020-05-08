39c2ddfdb2a6111fa4a489ed5b9715d6996311fa
Adrian Tamas <adi.tamas@outlook.com>
2020-05-08 12:36:53 +0300

    add extension for windows

--- pygeckodriver/__init__.py
+++ pygeckodriver/__init__.py
@@ -14,20 +14,24 @@ def _get_filename():
 
     sys = platform.system()
     arch = platform.machine()
+    file_extension = None
     if sys == 'Darwin':
         path += 'macos'
     else:
         if sys == 'Windows':
             path += 'win'
+            file_extension = ".exe"
         elif sys == 'Linux':
             path += 'linux'
         else:
             raise Exception('OS not supported')
         path += '64' if arch.endswith('64') else '32'
+        path += file_extension
 
     if not os.path.exists(path):
         raise FileNotFoundError('GeckoDriver for {}({}) '
-                'is not found.'.format(sys, arch))
+                                'is not found.'.format(sys, arch))
     return path
+
 
 geckodriver_path = _get_filename()
