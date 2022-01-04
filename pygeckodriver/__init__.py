8f500104d8992490ce2ef199de72df64bcbf8a93
Valeriy Ryaboshapko <valera@replika.ai>
2022-01-04 19:33:12 +0300

    make executable name a bit more explicit

--- pygeckodriver/__init__.py
+++ pygeckodriver/__init__.py
@@ -11,20 +11,19 @@ _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def _get_filename():
-    path = os.path.join(_BASE_DIR, 'geckodriver_')
-
     arch = '64' if platform.machine().endswith('64') else '32'
 
     sys = platform.system()
     if sys == 'Darwin':
-        path += 'macos'
+        file_name = 'geckodriver_macos'
     elif sys == 'Windows':
-        path += 'win{}.exe'.format(arch)
+        file_name = 'geckodriver_win{}.exe'.format(arch)
     elif sys == 'Linux':
-        path += 'linux{}'.format(arch)
+        file_name = 'geckodriver_linux{}'.format(arch)
     else:
         raise Exception('OS {} not supported'.format(sys))
 
+    path = os.path.join(_BASE_DIR, file_name)
     if not os.path.exists(path):
         raise FileNotFoundError('GeckoDriver for {}({}) '
                                 'is not found.'.format(sys, arch))
